85c5348a04caf98bc1a710905ceae3fb79b07d4c
lazerblasters <40807667+lazerblasters@users.noreply.github.com>
2021-09-14 23:20:21 -0400

    able to send notifications

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const shell = require('shelljs');
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  const match = req.query.match === 'true';
+  const message = {
+    notification: {
+      title: 'Test',
+      body: 'Test',
+    },
+    tokens: match ? ['something'] : ['another'],
+  }
+  shell.exec(`echo '{ "aps": { "alert": ${JSON.stringify(message.notification)}, "tokens": ${JSON.stringify(message.tokens)} } }' | xcrun simctl push booted com.example.pushnotification -`);
+  res.send('')
 })
 
 app.listen(port, () => {
